c8ca7ee30c2349b046a05da1487d82220c2bebd8
unknown <Katherine2bk@gmail.com>
2021-06-26 00:35:04 -0700

    finished router quickly, now to test middleware

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,46 @@
-// DO YOUR MAGIC
+const express = require('express');
+const router = express.Router()
+const Cars = require('./cars-model');
+const {checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid} = require('./cars-middleware');
+
+//get
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Cars.getAll()
+        res.json(cars)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// getById
+router.get('/:id', checkCarId, async (req, res, next) => {
+    try {
+        res.status(200).json(req.cars)
+    } catch (error) {
+        next(error)
+    }
+})
+
+//create car
+
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next) => {
+    try {
+        res.status(201).json(req.body)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+server.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.render({
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require("express")
 // cars router
+const carsRouter = require('./cars/cars-router');
 const server = express()
 server.use(express.json())
+
+server.use('/api/cars', carsRouter)
 
 server.get('/', (req, res) => {
     res.send('Welcome to the API')
